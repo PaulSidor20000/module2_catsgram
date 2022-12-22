ad9ba8494ae492441636e808faf8a9a90ff244db
PaulSidor20000 <pavel.sidoroff2014@yandex.ru>
2022-12-22 21:36:02 +0300

    refactor: add class User

---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,41 @@
+public class User {
+    private String email;
+    private String nickname;
+    private LocalDate birthdate;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        if (!super.equals(object)) return false;
+        User user = (User) object;
+        return java.util.Objects.equals(email, user.email);
+    }
+
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), email);
+    }
+}
